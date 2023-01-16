1196e0dc0b31de2c3c9530d35a79833e75b5e312
Victoria <95590803+anarayner@users.noreply.github.com>
2023-01-15 20:43:02 -0500

    location, apiurl

--- src/store/dataStore.ts
+++ src/store/dataStore.ts
@@ -85,7 +85,7 @@ class DataStore {
                 const loc = await axios.get(`http://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid=${API_KEY}`)
                 location = loc.data.name
                 console.log(location)
-                if(!this._location) this.setLocation(location)
+                if(!this._location) this.setLocation('Chattanooga')
             }
 
             const data = await fetchAllWeather(location)
